09ac0e0c9ac7c6905abb0919595cb299a1851568
RubenEngel <66388962+RubenEngel@users.noreply.github.com>
2020-07-07 21:33:04 +0100

    Added Browser Router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/styles.css';
 import App from './App';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+    </Switch>
+  </Router>
+    
+
+  ,document.getElementById('root')
 );
